c49376f6407167f8a4c8dcdc6eac2249d7035698
Masahiro Honma <hiratara@cpan.org>
2012-07-01 10:21:56 +0900

    more samples

---
+++ TypeConstraint.hs
@@ -0,0 +1,13 @@
+{-# LANGUAGE DatatypeContexts #-}
+
+data (Ord a) => OrdStack a = Bottom | Item a (OrdStack a)
+                             deriving (Show)
+
+isIncreasing :: (Ord a) => OrdStack a -> Bool
+isIncreasing (Item a rest@(Item b _))
+                 | a < b = isIncreasing rest
+                 | otherwise = False
+isIncreasing _ = True
+
+push :: (Ord a) => a -> OrdStack a -> OrdStack a
+push a s = Item a s
