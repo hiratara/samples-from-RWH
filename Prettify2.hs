e1439519b553aa6cf1a1ba3680d664f2b2766e2b
Masahiro Honma <hiratara@cpan.org>
2012-07-01 13:14:16 +0900

    more samples

--- Prettify2.hs
+++ Prettify2.hs
@@ -4,3 +4,23 @@ data Doc = Empty | Char Char | Text String | Line
                  | Concat Doc Doc | Union Doc Doc
                    deriving (Show, Eq)
 
+empty :: Doc
+empty = Empty
+
+(<>) :: Doc -> Doc -> Doc
+Empty <> d2 = d2
+d1 <> Empty = d1
+d1 <> d2 = Concat d1 d2
+
+char :: Char -> Doc
+char c = Char c
+
+text :: String -> Doc
+text "" = Empty
+text s  = Text s
+
+double :: Double -> Doc
+double d = text (show d)
+
+line :: Doc
+line = Line
--- QC.hs
+++ QC.hs
@@ -29,3 +29,13 @@ instance Arbitrary Doc where
                  liftM2 Concat arbitrary arbitrary,
                  liftM2 Union arbitrary arbitrary
                 ]
+
+prop_empty_id x = empty <> x == x
+                  &&
+                  x <> empty == x
+
+prop_char c = char c == Char c
+prop_text s = text s == if null s then Empty else Text s
+prop_line = line == Line
+prop_double d = double d == text (show d)
+
