c6457f56d6bda92c04d9bb36478356fbd27affd8
Masahiro Honma <hiratara@cpan.org>
2012-07-02 20:17:07 +0900

    more examples

---
+++ funcrecs2.hs
@@ -0,0 +1,13 @@
+data FuncRec = FuncRec {
+      name :: String, calc :: Int -> Int, namedCalc :: Int -> (String, Int)
+}
+
+mkFuncRec :: String -> (Int -> Int) -> FuncRec
+mkFuncRec name calcfunc = FuncRec {
+                            name = name, calc = calcfunc,
+                            namedCalc = \x -> (name, calcfunc x)
+}
+
+plus5 = mkFuncRec "plus5" (+ 5)
+always0 = mkFuncRec "always0" $ const 0
+
