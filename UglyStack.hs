2052f6cd0dd56f6bcd5a26e909485c971c1fb656
Masahiro Honma <hiratara@cpan.org>
2012-07-05 11:45:07 +0900

    more samples

---
+++ UglyStack.hs
@@ -0,0 +1,35 @@
+import System.Directory
+import System.FilePath
+import Control.Monad.Reader
+import Control.Monad.State
+import CountEntries (listDirectory)
+
+data AppConfig = AppConfig {
+      cfgMaxDepth :: Int
+    } deriving (Show)
+
+data AppState = AppState {
+      stDeepestReached :: Int
+    } deriving (Show)
+
+type App = ReaderT AppConfig (StateT AppState IO)
+
+constrainedCount :: Int -> FilePath -> App [(FilePath, Int)]
+constrainedCount curDepth path = do
+  contents <- liftIO . listDirectory $ path
+  (AppConfig cfg) <- ask
+  rest <- forM contents $ \name -> do
+            let newPath = path </> name
+            isDir <- liftIO $ doesDirectoryExist newPath
+            if isDir && curDepth < cfg
+              then do
+                let newDepth = curDepth + 1
+                (AppState st) <- get
+                when (st < newDepth) $
+                  put $ AppState newDepth
+                -- st <- get
+                -- when (stDeepestReached st < newDepth) $
+                --   put st { stDeepestReached = newDepth }
+                constrainedCount newDepth newPath
+              else return []
+  return $ (path, length contents) : concat rest
