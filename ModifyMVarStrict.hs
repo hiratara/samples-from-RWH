27c2bc1b62d316a18f9217dc20e96c3589d94237
Masahiro Honma <hiratara@cpan.org>
2012-07-09 13:38:42 +0900

    use mask instead of block and unblock

--- ModifyMVarStrict.hs
+++ ModifyMVarStrict.hs
@@ -1,12 +1,12 @@
 {-# LANGUAGE BangPatterns, ScopedTypeVariables #-}
 
 import Control.Concurrent (MVar, putMVar, takeMVar)
-import Control.Exception (SomeException, block, catch, throw, unblock)
+import Control.Exception (SomeException, catch, throw, mask)
 import Prelude hiding (catch) -- use Control.Exception's version
 
 modifyMVar_strict :: MVar a -> (a -> IO a) -> IO ()
-modifyMVar_strict m io = block $ do
+modifyMVar_strict m io = mask $ \restore -> do
   a <- takeMVar m
-  !b <- unblock (io a) `catch` \(e::SomeException) ->
+  !b <- restore (io a) `catch` \(e::SomeException) ->
         putMVar m a >> throw e
   putMVar m b
